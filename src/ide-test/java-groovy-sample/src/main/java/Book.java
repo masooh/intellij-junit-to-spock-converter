006899a488d314d667310df12893025d42ecd1a9
masooh <martin_hofmann_de@yahoo.de>
2018-11-17 11:05:07 +0100

    add maven project for manual IDE tests

---
+++ src/ide-test/java-groovy-sample/src/main/java/Book.java
@@ -0,0 +1,25 @@
+public class Book {
+    private String title;
+    private Integer pages;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        if (pages < 1) {
+            throw new IllegalArgumentException("pages must be greater 0");
+        }
+        this.pages = pages;
+    }
+
+
+}
