5ca41db95761e44142230a7b8fc0ab27425f11a1
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-02-25 17:38:09 +0100

    WIP: Add first files for testing

---
+++ src/test/kotlin/com/github/masooh/intellij/plugin/groovyfier/ConvertJavaToGroovyTest.kt
@@ -0,0 +1,11 @@
+package com.github.masooh.intellij.plugin.groovyfier
+
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+
+class ConvertJavaToGroovyTest : LightJavaCodeInsightFixtureTestCase() {
+
+
+    fun testName() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
