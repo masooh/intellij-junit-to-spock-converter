b1c44b7f16729b32d2ec7a6e6cb5e6eaec42fdac
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-03-26 22:22:01 +0100

    more test trials

--- src/test/kotlin/com/github/masooh/intellij/plugin/junitspock/ConvertJUnitToSpockTest.kt
+++ src/test/kotlin/com/github/masooh/intellij/plugin/junitspock/ConvertJUnitToSpockTest.kt
@@ -1,6 +1,9 @@
 package com.github.masooh.intellij.plugin.junitspock
 
-import com.github.masooh.intellij.plugin.junitspock.action.ConvertJUnitToSpock
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.psi.PsiFile
+import com.intellij.testFramework.TestActionEvent
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 
 // no virtual file
@@ -10,8 +13,27 @@ class ConvertJUnitToSpockTest : LightJavaCodeInsightFixtureTestCase() {
     override fun getTestDataPath() = "src/test/resources/testdata"
 
     fun testName() {
-        myFixture.configureByFile("BookTest.java")
-        myFixture.testAction(ConvertJUnitToSpock())
-        myFixture.checkResultByFile("BookTest.groovy")
+        // copies from #getTestDataPath to test project and opens in editor
+        val psiFile = myFixture.configureByFile("BookTest.groovy") // returns PSI file
+
+        // todo psiFile is not recognized as Groovy but Plain Text, Groovy is not in list of FileType type = myPatternsTable.findAssociatedFileType(fileName);
+//        myFixture.testAction(ConvertJavaToGroovy())
+        testAction(ConvertJavaToGroovy(), psiFile)
+        myFixture.checkResultByFile("BookTestTransformed.groovy")
+    }
+
+    /** inspired by myFixture.testAction
+     */
+    private fun testAction(action: AnAction, psiFile: PsiFile): Presentation? {
+        val e = TestActionEvent(action) // hier Rückbezug was passiert damit?
+//        action.beforeActionPerformedUpdate(e)
+        if (e.presentation.isEnabled && e.presentation.isVisible) {
+//            action.actionPerformed(e)
+            // an dieser Stelle gehen wir schon davon aus, dass es eine Groovy Datei ist
+            // -> Editor beibringen, dass Inhalt Groovy ist
+            // GroovyFixesApplier.applyGroovyFixes(event) geht evtl. auch
+            JUnitToSpockApplier(e, psiFile).transformToSpock()
+        }
+        return e.presentation
     }
 }
--- src/test/resources/testdata/BookTestTransformed.groovy
+++ src/test/resources/testdata/BookTestTransformed.groovy
@@ -1,21 +1,9 @@
-package junit4;
+package junit4
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import org.junit.*
+import sample.Book
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import sample.Book;
+import static org.junit.Assert.*
 
 public class BookTest {
 
