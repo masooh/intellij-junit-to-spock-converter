7de1e909130faf0a10a0f852f64d47e43b14b1c5
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-07-30 15:43:14 +0200

    Add SpringTest to sample project

---
+++ src/ide-test/java-groovy-sample/src/test/java/junit4/SpringTest.java
@@ -0,0 +1,34 @@
+package junit4;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import sample.Book;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = SpringTest.Config.class)
+public class SpringTest {
+
+    static class Config {
+        @Bean
+        Book cleanCode() {
+            Book book = new Book();
+            book.setTitle("Clean Code");
+            return book;
+        }
+    }
+
+    @Autowired
+    Book book;
+
+    @Test
+    public void injectionWorks() {
+        Assert.assertNotNull(book);
+
+        Assert.assertEquals("Clean Code", book.getTitle());
+    }
+}
